07568d4ff05e753bd1bb042b297e8c58f7ecb17a
Migas77 <miguel.belchior@ua.pt>
2026-04-20 21:55:16 +0100

    test(rates-controller): testing controller with mock rates service
    Test Cases:
    - valid request with query parameter 'from' only
    - valid request with query parameter 'from' and 'to' specified
    - invalid request with invalid query parameters (using ParameterizedTest)
    - unexpeted exception handled and returned as a ProblemDetail with status 500 (by GlobalExceptionHandler)

---
+++ src/test/java/com/miguelbf/exchangerateapi/controller/ExchangeRatesControllerMockServiceTest.java
@@ -0,0 +1,155 @@
+package com.miguelbf.exchangerateapi.controller;
+
+import com.miguelbf.exchangerateapi.domain.enums.Currency;
+import com.miguelbf.exchangerateapi.domain.record.RatesResponse;
+import com.miguelbf.exchangerateapi.service.IExchangeRatesService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ExchangeRatesController.class)
+public class ExchangeRatesControllerMockServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private IExchangeRatesService exchangeRatesService;
+
+    @Test
+    void whenOnlyFromQueryParameter_thenStatusOkAndReturnAllRates() throws Exception {
+        when(exchangeRatesService.getRates(Currency.USD, null))
+            .thenReturn(new RatesResponse(1L, Currency.USD, Map.of(
+                Currency.EUR, new BigDecimal("0.85"),
+                Currency.GBP, new BigDecimal("0.74"),
+                Currency.JPY, new BigDecimal("158.74")
+            )));
+
+        mockMvc
+            .perform(
+                get("/api/rates")
+                .queryParam("from", Currency.USD.name())
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.timestamp", is(1)))
+            .andExpect(jsonPath("$.source", is(Currency.USD.name())))
+            .andExpect(jsonPath("$.rates", aMapWithSize(3)))
+            .andExpect(jsonPath("$.rates.EUR", is(0.85)))
+            .andExpect(jsonPath("$.rates.GBP", is(0.74)))
+            .andExpect(jsonPath("$.rates.JPY", is(158.74)));
+
+        verify(exchangeRatesService, times(1)).getRates(Currency.USD, null);
+    }
+
+    @Test
+    void whenFromAndToQueryParameters_thenStatusOkAndReturnSingleRate() throws Exception {
+        // shall not reject the request despite from and to query parameters being the same
+        when(exchangeRatesService.getRates(Currency.EUR, Currency.EUR))
+            .thenReturn(new RatesResponse(1L, Currency.EUR, Map.of(Currency.EUR, new BigDecimal("1.0"))));
+
+        mockMvc
+            .perform(
+                get("/api/rates")
+                .queryParam("from", Currency.EUR.name())
+                .queryParam("to", Currency.EUR.name())
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.timestamp", is(1)))
+            .andExpect(jsonPath("$.source", is(Currency.EUR.name())))
+            .andExpect(jsonPath("$.rates", aMapWithSize(1)))
+            .andExpect(jsonPath("$.rates.EUR", is(1.0)));
+
+        verify(exchangeRatesService, times(1)).getRates(Currency.EUR, Currency.EUR);
+    }
+
+    @ParameterizedTest( name = "[{index}] from={0} to={1}")
+    @MethodSource("invalidQueryParams")
+    void whenInvalidQueryParameters_thenStatusBadRequestAndReturnProblemDetail(Object from, Object to) throws Exception {
+        // Default Problem Detail handled by Spring
+
+        MockHttpServletRequestBuilder request = get("/api/rates").contentType(MediaType.APPLICATION_JSON);
+        if (from != null) request = request.queryParam("from", from.toString());
+        if (to != null) request = request.queryParam("to", to.toString());
+
+        mockMvc
+            .perform(request)
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.instance", is("/api/rates")))
+            .andExpect(jsonPath("$.title", is("Bad Request")))
+            .andExpect(jsonPath("$.status", is(HttpStatus.BAD_REQUEST.value())))
+            .andExpect(jsonPath("$.detail", not(emptyOrNullString())));
+
+        verify(exchangeRatesService, never()).getRates(any(),any());
+    }
+
+    @Test
+    void whenUnexpectedException_thenStatusInternalServerErrorAndReturnProblemDetail() throws Exception {
+        // Generic Problem Detail handled by GlobalExceptionHandler
+        when(exchangeRatesService.getRates(any(), any())).thenThrow(new RuntimeException("Unexpected error"));
+
+        mockMvc
+            .perform(
+                get("/api/rates")
+                .queryParam("from", Currency.USD.name())
+                .queryParam("to", Currency.EUR.name())
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError())
+            .andExpect(jsonPath("$.instance", is("/api/rates")))
+            .andExpect(jsonPath("$.title", is("Internal Server Error")))
+            .andExpect(jsonPath("$.status", is(HttpStatus.INTERNAL_SERVER_ERROR.value())))
+            .andExpect(jsonPath("$.detail", not(emptyOrNullString())));
+
+        verify(exchangeRatesService, times(1)).getRates(any(), any());
+    }
+
+
+    private static Stream<Arguments> invalidQueryParams() {
+        List<Object> validFroms = List.of(Currency.USD);
+        List<Object> validTos   = List.of(Currency.EUR);
+        List<Object> invalidFroms = Arrays.asList(null, "zzz", -1, "%s,%s".formatted(Currency.USD, Currency.EUR));
+        List<Object> invalidTos   = Arrays.asList("zzz", -1, "%s,%s".formatted(Currency.USD, Currency.EUR));
+
+        Stream<Arguments> invalidFromWithValidTo = allCombinations(invalidFroms, validTos);
+        Stream<Arguments> validFromWithInvalidTo = allCombinations(validFroms, invalidTos);
+
+        return Stream.concat(invalidFromWithValidTo, validFromWithInvalidTo);
+    }
+
+    private static Stream<Arguments> allCombinations(List<?>... lists) {
+        return Arrays.stream(lists)
+            .reduce(
+                Stream.of(Collections.emptyList()),
+                (combinations, list) -> combinations
+                    .flatMap(combo -> list.stream()
+                        .map(value -> {
+                            List<Object> next = new ArrayList<>(combo);
+                            next.add(value);
+                            return next;
+                        })
+                    ),
+                Stream::concat
+            )
+            .map(combo -> Arguments.of(combo.toArray()));
+    }
+}
